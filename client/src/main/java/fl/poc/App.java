98cc82781fc42c68a6c170962c19bb5f01369f48
F.List <36734596+flistell@users.noreply.github.com>
2024-06-19 15:14:46 +0200

    working with JDK 8. NOT WORKING WITH JDK11

--- client/src/main/java/fl/poc/App.java
+++ client/src/main/java/fl/poc/App.java
@@ -6,7 +6,10 @@ import javax.xml.ws.WebServiceRef;
 
 public class App {
 
-    //@WebServiceRef(wsdlLocation = "http://127.0.0.1:7001/ws-security-sample-0.0.1-SNAPSHOT/PingService?WSDL")
+    /*
+    @WebServiceRef(wsdlLocation = "http://127.0.0.1:7001/ws-security-sample-0.0.1-SNAPSHOT/PingService?WSDL")
+    private PingService service;
+    */
     
 
     public static void main(String[] args){
